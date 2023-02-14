7e43bc3e77368fb6c608a73980f266f1e0c09381
arewabolu <bolu.arewa@gmail.com>
2023-02-14 12:38:31 +0100

    dependencies and test

---
+++ go.mod
@@ -0,0 +1,13 @@
+module mygoML
+
+go 1.19
+
+require (
+	github.com/arewabolu/csvmanager v1.0.0
+	golang.org/x/net v0.5.0
+)
+
+require (
+	golang.org/x/exp v0.0.0-20230113213754-f9f960f08ad4
+	gonum.org/v1/gonum v0.12.0
+)
---
+++ main_test.go
@@ -0,0 +1,36 @@
+package tradefuncs
+
+import (
+	"testing"
+
+	csv "github.com/arewabolu/csvmanager"
+	"gonum.org/v1/gonum/stat"
+)
+
+func TestMax(t *testing.T) {
+	max := max(1.0, 2.0, 3.0)
+	if max != 3.0 {
+		t.Error("Max failed")
+	}
+}
+
+func TestAggregate(t *testing.T) {
+	records, _ := csv.ReadCsv("./BTCUSDT-1h-2022-11.csv")
+	high := records.Col("high").Float()
+	//	open := records.Col("open").Float() //,,,
+
+	low := records.Col("low").Float()
+	close := records.Col("close").Float()
+	_ = MACDVs(close, high, low)
+}
+
+func TestMeanDiff(t *testing.T) {
+	rd, _ := csv.ReadCsv("./csvfiles/MACD.csv")
+	MACDV := rd.Col("MACDV").Float()
+	MACDVmean := stat.Mean(MACDV, nil)
+	up, down, _ := meanDiff(MACDVmean, MACDV)
+	if up == 0 || down == 0 {
+		t.Error("MACDV data must be incorrect")
+	}
+
+}
